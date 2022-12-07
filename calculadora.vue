be9ac2d01bf820563cb2173b9d99e8ad8450d1e7
João Leonardo <joaoleonardo9921@gmail.com>
2022-12-07 19:46:58 -0300

    commit de rotina

---
+++ calculadora.vue
@@ -0,0 +1,38 @@
+<!--
+<template>
+ <div id="app">
+        
+ </div>
+</template>
+
+<script> //Não implementado ainda
+    const vm = new Vue(
+        {
+            el:'#app',
+            data: {
+                n1: Number,
+                n2: Number
+            },
+            methods: {
+                soma(n1=0, n2=0){
+                    return n1 + n2;
+                },
+                subtrai(n1=0, n2=0){
+                    return n1 - n2;
+                },
+                multiplica(n1=0, n2=0){
+                    return n1 * n2;
+                },
+                divide(n1=0, n2=0){
+                    return n1 / n2;
+                },
+            }
+    }
+
+    )
+</script>
+
+<style scoped>
+
+</style>
+-->
